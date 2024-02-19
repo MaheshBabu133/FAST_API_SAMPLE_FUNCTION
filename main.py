786ee156bceb3193c4bc9a7ba23f24d6db79c17a
MaheshBabu133 <maheshbabuntr123@gmail.com>
2024-02-19 22:31:35 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI() #Object instance
+
+
+'''
+@app: path operation decorator
+get : Operation
+("/") : base path
+function : path operation function
+'''
+
+@app.get("/")  #Decorator
+def sample(): 
+    return {"a":[1,2,3],'b':{'a':1,'b':2,'c':True,'d':False}}
+
+
+@app.get("/myfunction")
+def sample():
+    return {"Nithish":"Good boy",'Love':{"Sowjanya":"Good gitl",'swathi':'short girl','sonu':'my wife'}}
+
+@app.get("/function")
+def sample2():
+    return "Mahesh and sonu are husband and wife"
+
